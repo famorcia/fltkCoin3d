11734bfd1792987820e60ebade5e9a68ff7c8c27
Fabrizio Morciano <>
2025-12-21 22:04:45 +0100

    feat: initialize FLTK project structure and basic OpenGL window
    - Set up `main.cpp` with a standard FLTK event loop and window initialization.
    - Implement `MyGLWindow` class inheriting from `Fl_Gl_Window` in `Coin3DCanvas.cpp`.
    - Added a basic OpenGL `draw()` implementation that renders a colored triangle to verify the graphics pipeline.
    - Preserved legacy wxWidgets and Coin3D code in comments for future porting reference.

---
+++ README.md
@@ -0,0 +1,7 @@
+# wxCoin3D
+Porting on fltk of Coin3D (a.k.a Open Inventor) examples
+
+## Compilation
+These examples require installation of Coin (at least 4.0) and fltk (at least 1.4.4)
+
+    cmake . -B build
---
+++ minimal/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * wxCoin3D Porting on wxWidgets of Coin3D (a.k.a Open Inventor) examples
+ * Copyright (C) 2022  Fabrizio Morciano
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#include <FL/Fl.H>
+
+#include "Coin3DCanvas.h"
+
+int main(int argc, char** argv) {
+    Fl_Window win(640, 480, "FLTK + OpenGL Example");
+    MyGLWindow glwin(10, 10, 620, 460);
+    win.end();
+    win.show(argc, argv);
+    return Fl::run();
+}
+
